9aa20971a1f981d8ec0d5f4de6a82e197917ea35
Awais JMM <jmm.awaiskiyani@gmail.com>
2025-01-17 16:11:00 +0500

    Add project files.

---
+++ BlazorAccountsApp/Model/Account.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorAccountsApp.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public AccountType? Types { get; set; }
+        public int? AccountCode { get; set; }
+        public int? ParentAcccountId { get; set; }
+        public Account? account { get; set; }
+        public decimal? debit { get; set; }
+        public decimal? credit { get; set; }
+        public bool? IsDeleted { get; set; }
+        public decimal? OpeningBalance { get; set; }
+    }
+
+    public enum AccountType
+    {
+        Assets,
+        Liability,
+        Revenue,
+        Expense,
+        Equity
+    }
+}
